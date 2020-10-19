0a8005f57a40adccd1bc1060f24fd6a452edd76f
estel <estel@DESKTOP-44S7CP6>
2020-10-19 20:42:03 +0200

    creerClientPM + creerClientPP

--- projet/src/projet/model/Client.java
+++ projet/src/projet/model/Client.java
@@ -14,5 +14,18 @@ public class Client extends Individu{
 
     public Client(String mdpI, Personne pers) {
         super(mdpI, pers);
-    }   
+    }
+    
+    //client PP
+    public void creerClientPP(String mdpI, String nomPP, String prenomPP, String adressePP, String cPPP, String villePP, Long telPP, String emailPP ){
+        PersPhysique PP = new PersPhysique(adressePP, villePP, cPPP, telPP, emailPP, nomPP, prenomPP);
+        Client CPP = new Client(mdpI, PP);
+    }
+    
+    public void creerClientPM(String mdpI, String nomSoc, Long numSiret, String adressePM, String cPPM, String villePM, Long telPM, String emailPM){
+        PersMorale PM = new PersMorale(adressePM, villePM, cPPM, telPM, emailPM, numSiret, nomSoc);
+        Client CPM = new Client(mdpI, PM);
+    }
+    
+    
 }
