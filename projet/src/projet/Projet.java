5430aed1bbafdaea26193035f5a8f1202461572a
estel <estel@DESKTOP-44S7CP6>
2020-10-19 19:20:28 +0200

    VérificationAutoIncrement

--- projet/src/projet/Projet.java
+++ projet/src/projet/Projet.java
@@ -5,6 +5,11 @@
  */
 package projet;
 
+import projet.model.Client;
+import projet.model.Intervenant;
+import projet.model.PersMorale;
+import projet.model.PersPhysique;
+
 /**
  *
  * @author tutu
@@ -16,7 +21,22 @@ public class Projet {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       
+        //nouvelle personne physique 
+        PersPhysique PP1 = new PersPhysique("18 rue Jean Durroux", "Foix", "09000",6812312345678L , "abc@outlook.fr", "Dupont", "Charles");
+        System.out.println(PP1.getCodeP());
         
+        PersPhysique PP2 = new PersPhysique("18 rue Jean Durroux", "Foix", "09000",6812312345678L , "abc@outlook.fr", "Dupont", "Charles");
+        System.out.println(PP2.getCodeP());
+        
+        PersMorale PM1 = new PersMorale("59 rue Jean", "Carcassonne", "11000",687654321L ,"def@outlook.fr",12345678912345L, "SociétéDupuis&Co");
+        System.out.println(PP1.getCodeP());
+        
+        Client C1 = new Client("blabla",PP1);
+        System.out.println(C1.getCodeI());
+        
+        Intervenant I1 = new Intervenant("FR7618523697845", "abcdef", PM1);
+        System.out.println(C1.getCodeI());
         
     }
     
