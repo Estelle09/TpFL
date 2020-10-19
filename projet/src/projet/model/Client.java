8eae978f001ecd597d627c894b744dedcdddd1de
estel <estel@DESKTOP-44S7CP6>
2020-10-19 20:53:07 +0200

    commentaires

--- projet/src/projet/model/Client.java
+++ projet/src/projet/model/Client.java
@@ -16,16 +16,19 @@ public class Client extends Individu{
         super(mdpI, pers);
     }
     
-    //client PP
+    // si nouvelle PP --> Création client
     public void creerClientPP(String mdpI, String nomPP, String prenomPP, String adressePP, String cPPP, String villePP, Long telPP, String emailPP ){
         PersPhysique PP = new PersPhysique(adressePP, villePP, cPPP, telPP, emailPP, nomPP, prenomPP);
         Client CPP = new Client(mdpI, PP);
     }
     
+    // si nouvelle PM --> Création client
     public void creerClientPM(String mdpI, String nomSoc, Long numSiret, String adressePM, String cPPM, String villePM, Long telPM, String emailPM){
         PersMorale PM = new PersMorale(adressePM, villePM, cPPM, telPM, emailPM, numSiret, nomSoc);
         Client CPM = new Client(mdpI, PM);
     }
+    
+    
     
     
 }
