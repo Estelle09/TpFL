72b1c6459eda8c8b2ba40cd014f9b32db2c91313
estel <estel@DESKTOP-44S7CP6>
2020-10-19 18:53:53 +0200

    AjoutClassePlateforme

---
+++ projet/src/projet/model/Plateforme.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author estel
+ */
+public class Plateforme {
+    private ArrayList<Individu> lstInscrit; 
+
+    public Plateforme(){
+        this.lstInscrit= new ArrayList<>();   
+    }
+    
+   
+    
+   
+    
+    
+
+}
+
+
