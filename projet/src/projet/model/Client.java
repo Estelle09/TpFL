431764c0aa125880280fc666ac210f23371ae178
estel <estel@DESKTOP-44S7CP6>
2020-10-19 21:25:49 +0200

    Suppression fonction inutile de création de client

--- projet/src/projet/model/Client.java
+++ projet/src/projet/model/Client.java
@@ -17,7 +17,7 @@ public class Client extends Individu{
     }
     
     // si nouvelle PP --> Création client
-    public void creerClientPP(String mdpI, String nomPP, String prenomPP, String adressePP, String cPPP, String villePP, Long telPP, String emailPP ){
+    /*public void creerClientPP(String mdpI, String nomPP, String prenomPP, String adressePP, String cPPP, String villePP, Long telPP, String emailPP ){
         PersPhysique PP = new PersPhysique(adressePP, villePP, cPPP, telPP, emailPP, nomPP, prenomPP);
         Client CPP = new Client(mdpI, PP);
     }
@@ -26,7 +26,7 @@ public class Client extends Individu{
     public void creerClientPM(String mdpI, String nomSoc, Long numSiret, String adressePM, String cPPM, String villePM, Long telPM, String emailPM){
         PersMorale PM = new PersMorale(adressePM, villePM, cPPM, telPM, emailPM, numSiret, nomSoc);
         Client CPM = new Client(mdpI, PM);
-    }
+    }*/
     
     
     
